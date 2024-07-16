edf373d1e9f51ccdaa438eee2678bb43b2acea02
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-16 11:00:01 +0530

    Creating a basic backend app

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
