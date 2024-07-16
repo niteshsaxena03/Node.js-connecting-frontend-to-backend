f3e5af1a6f3b12c311d8b014d5df2b475be0b2e3
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-07-16 11:06:05 +0530

    Adding a jokes route in backend

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,38 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "Why did the scarecrow win an award?",
+      content: "Because he was outstanding in his field!",
+    },
+    {
+      id: 2,
+      title: "What do you call fake spaghetti?",
+      content: "An impasta!",
+    },
+    {
+      id: 3,
+      title: "Why don't scientists trust atoms?",
+      content: "Because they make up everything!",
+    },
+    {
+      id: 4,
+      title: "What do you get if you cross a snowman and a vampire?",
+      content: "Frostbite.",
+    },
+    {
+      id: 5,
+      title: "Why don’t skeletons fight each other?",
+      content: "They don’t have the guts.",
+    },
+  ];
+
+  res.send(jokes);
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
